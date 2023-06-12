799f80cd629b26033265d78fe684a75099caad18
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-06-12 16:08:30 +0200

    Auth middleware added

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,10 @@
 import { Signup, Login } from "../Controllers/UserController.js";
 import express from "express";
+import { userVerification } from "../Middlewares/UserMiddleware.js";
 
 const router = express.Router()
 
+router.post('/',userVerification)
 //signup route
 router.post("/signup", Signup);
 
